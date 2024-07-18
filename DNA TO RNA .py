71eb27c85618ecedb0a07c801754cc55a158e5ef
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-18 13:27:29 -0400

    Update and rename DNA TO RNA .py to Transcribing DNA into RNA

--- DNA TO RNA .py
+++
@@ -1,8 +0,0 @@
-dna_nucleotides='GATGGAACTTGACTACGTAAATT'
-def dna_to_rna(dna_nucleotides):
-    rna_nucleotides=''
-    for i in dna_nucleotides:
-        rna_nucleotides=dna_nucleotide.replace('T', 'U')
-        return rna_nucleotides
-print(dna_to_rna(dna_nucleotides))
-        
